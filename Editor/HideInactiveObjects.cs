c108aae518635dbe576529e60d8aa043a2e734d4
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-09-23 21:30:04 +0900

    initial commit

---
+++ Editor/HideInactiveObjects.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class HideInactiveObjects
+{
+    private static bool isHiddenObjects = true;
+    private const string MenuPath = "Tools/Hide Inactive Objects";
+
+    static GameObject[] GetInactiveObjects()
+    {
+        GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+        GameObject[] inactiveObjects = Array.FindAll(allObjects, x => (!x.activeInHierarchy && !x.activeSelf));
+        return inactiveObjects;
+    }
+
+    static void HideObjects(GameObject[] targets)
+    {
+        
+        foreach (var target in targets)
+        {
+            target.hideFlags = HideFlags.HideInHierarchy;
+        }
+        // targets.Select(x => (x.hideFlags = HideFlags.HideInHierarchy));
+    }
+
+    static void DisplayObjects(GameObject[] targets)
+    {
+        
+        foreach (var target in targets)
+        {
+            target.hideFlags = HideFlags.None;
+        }
+        // targets.Select(x => (x.hideFlags = HideFlags.None));
+    }
+    
+    [MenuItem(MenuPath)]
+    static void ToggleOption()
+    {
+        if (isHiddenObjects)
+        {
+            Debug.Log(GetInactiveObjects().Length);
+            HideObjects(GetInactiveObjects());
+        }
+        else
+        {
+            DisplayObjects(GetInactiveObjects());
+        }
+        
+        isHiddenObjects = !isHiddenObjects;
+        Menu.SetChecked(MenuPath, isHiddenObjects);
+    }
+    
+    [MenuItem(MenuPath, true)]
+    static bool ValidateOption()
+    {
+        Menu.SetChecked(MenuPath, isHiddenObjects);
+        return true;
+    }
+}
